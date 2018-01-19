4b3a2ef6c3b6f64017bc92a3648c319564f7f36e
Matt Hoyle <code@deployable.co>
2018-01-19 01:21:33 +0000

    Add jsdoc

--- src/ClassDebug.js
+++ src/ClassDebug.js
@@ -3,27 +3,44 @@ import debugr from 'debug'
 const debug = debugr('mh:class-debug')
 const noop = function(){}
 
+/** Class to manage debug on other classes */
+export class ClassDebug {
 
-export default class ClassDebug {
-
+  /** 
+   * Initialise the class
+   */
   static classInit(){
-    // Set a generic prefix undefined
+    // Set a generic prefix var, so users can subclass in the their app (see `ClassDebug.extend()`)
     this.default_prefix = 'mh'
 
     // Create a store for debugs, so it can be possible to retroactively 
     // enable/disable the noop to avoid after startup. 
     // This is to avoid the the `debug` function call overhead
     this.debugs = {}
+
+    return this
   } 
 
-  // Complete setup for a module
+  /**
+   * Complete setup for a module
+
+   * @param {Class}  cls - Class to attach debug to
+   * @param {string} prefix - debug namespace prefix
+   */
   static setup( cls, prefix ){
-    const _Debug = new this( prefix )
-    _Debug.attach(cls)
-    return _Debug
+    const a_debug = new this( prefix )
+    a_debug.attach(cls)
+    return a_debug
   }
 
-  // Extend this class to include a custom prefix for a module
+  /**
+   * Extend this class to include a custom prefix for an external
+   * module to use. 
+
+   * @param {string} prefix - debug namespace prefix
+   * @param {string} name - Javascript name of the class
+   * @return {Class} The extended ClassDebug class
+   */
   static extend( prefix, name = 'Debug' ){
     let classes = {}
     classes[name] = class extends this {}
@@ -32,7 +49,13 @@ export default class ClassDebug {
     return classes[name]
   }
 
-  // Attach debug to a class
+  /**
+   * @description Attach a debug namespace to a class
+   * @param {Class}  cls - Class to attach debug to
+   * @param {string} prefix - debug namespace prefix
+   * @param {string} suffix - debug namespace suffix, defaults to the class name
+   * @return {object} The namespaced debug function
+  */
   static attach( cls, prefix, suffix ){
 
     if ( prefix === undefined || prefix === null ) {
@@ -52,7 +75,13 @@ export default class ClassDebug {
     return this.debugs[tag] = cls_debug
   }
 
-  // Attach a debug to a class instance, with optional [data]
+  /**
+   * @description Attach a debug to a class instance, with optional `[data]` after the `prefix:suffix`
+   * @param {object} instance - A class instance to attach debug to
+   * @param {string} prefix - debug namespace prefix
+   * @param {string} data - debug namespace data (after suffix)
+   * @return {object} The namespaced debug function
+   */
   static instance( instance, prefix, data ){
     let cls = instance.constructor
     debug('Attaching debug "%s" to class "%s" instance with data "%s"', prefix, cls.name, data)
@@ -68,18 +97,33 @@ export default class ClassDebug {
   }
 
   
-  // Create a debug instance with a set prefix
+  /**
+   * Create a debug instance with a set prefix
+
+   * @param {string} prefix - debug namespace prefix
+   */
   constructor( prefix ){
     this.prefix = prefix || this.constructor.default_prefix
   }
 
-  // Attach this debug to a class
-  attach( cls ){
-    return this.constructor.attach(cls, this.prefix)
-    
+  /**
+   * Attach a debug namespace to a class: `prefix:suffix`
+   *
+   * @param {Class}  cls - Class to attach debug to
+   * @param {string} suffix - debug namespace suffix, defaults to the class name
+   * @return {object} The namespaced debug function
+  */
+  attach( cls, suffix ){
+    return this.constructor.attach(cls, this.prefix, suffix)
   }
 
-  // Attach this debug to a class instance, with optional [data]
+  /**
+   * Attach a debug namespace to a class instance, with optional: `prefix:suffix[data]`
+   *
+   * @param {Class}  cls - Class to attach debug to
+   * @param {string} data - debug namespace data, defaults to the class name
+   * @return {object} The namespaced debug function
+  */
   instance( instance, data ){
     return this.constructor.instance(instance, this.prefix, data)
   }
@@ -87,3 +131,5 @@ export default class ClassDebug {
 }
 
 ClassDebug.classInit()
+
+export default ClassDebug
